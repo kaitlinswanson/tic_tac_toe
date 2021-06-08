be219dfe0fd71871f1872915691f36db0491b46f
Kaitlin Swanson <kateswanson94@gmail.com>
2021-06-08 12:11:14 -0700

    add index.html

---
+++ script.js
@@ -0,0 +1,17 @@
+// use a module to create the gameBoard object as an array inside of it. 
+
+const gameBoard = (function() => {
+  const cells = document.querySelectorAll('board');
+  let gridCells = ['', '', '', '', '', '', '', '', '']; 
+
+  const cellSelected = 
+
+   return {
+      
+        
+    };
+  })();
+
+//create displayController using module
+
+// use factories to create players
