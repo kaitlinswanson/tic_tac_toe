35f5e92b188299eb050a2fb01f3dbd6a566735cb
Kaitlin Swanson <kateswanson94@gmail.com>
2021-06-09 11:31:01 -0700

    add index.html

--- script.js
+++ script.js
@@ -1,17 +1,80 @@
+
+
 // use a module to create the gameBoard object as an array inside of it. 
 
-const gameBoard = (function() => {
-  const cells = document.querySelectorAll('board');
-  let gridCells = ['', '', '', '', '', '', '', '', '']; 
+//const gameBoard = (() => {
+// set up constants for winning combos, identefiers in the gameBoard and variable for the current turn.
+const xClass = 'x'
+const circleClass = 'circle';
+let circleBoard
+const cells = document.querySelectorAll('[data-cell]');
+const fullGameBoard = document.getElementById('gameBoardContainer')
+const winningSet = [
+  [0, 1, 2], 
+  [3, 4, 5], 
+  [6, 7, 8],
+  [0, 3, 6], 
+  [1, 4, 7], 
+  [2, 5, 8], 
+  [0, 4, 8], 
+  [2, 4, 6]
+]
 
-  const cellSelected = 
+startGame()
+// event listeners allowing the players selection to be clicked one time
+  function startGame() { 
+    circleTurn = false; 
+    
+    cells.forEach(cell => { 
+    cell.addEventListener('click', playerClick, {once: true})
+  }); 
+  setHover()
+  }
 
-   return {
-      
-        
-    };
-  })();
 
-//create displayController using module
+  function playerClick(e) {
+    const cell = e.target; 
+    const currentClass = circleBoard ? circleClass : xClass; 
+    //place marker
+    playerMarker(cell, currentClass)
 
-// use factories to create players
+//check to see if win
+
+
+//check for draw
+
+//switch back and forth between placer x and player circle
+    swapTurns()
+    setHover()
+  } 
+
+  function playerMarker(cell, currentClass) { 
+    cell.classList.add(currentClass)
+  }
+
+  function swapTurns() { 
+     circleBoard = !circleBoard;
+  }
+
+  function setHover() { 
+    fullGameBoard.classList.remove(xClass);
+    fullGameBoard.classList.remove(circleClass); 
+    if (circleBoard) { 
+      fullGameBoard.classList.add(circleClass); 
+    } else { 
+      fullGameBoard.classList.add(xClass); 
+    }
+    }
+  
+
+ 
+
+
+
+  
+ // })();
+
+//create a factrory function so you can add players
+const Player = (playerName, marker) => { 
+  
+}
